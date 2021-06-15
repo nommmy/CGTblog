3191a3f8b022de52da9d73e8eb4bb5637037f112
nomiyama <nomiyamade@gmail.com>
2021-06-15 12:33:54 +0900

    share機能追加

--- src/containers/templates/Article.tsx
+++ src/containers/templates/Article.tsx
@@ -12,6 +12,12 @@ import footnotes from 'remark-footnotes';
 import toc from 'remark-toc';
 import gfm from 'remark-gfm';
 import directive from 'remark-directive';
+import {
+  TwitterShareButton,
+  TwitterIcon,
+  FacebookShareButton,
+  FacebookIcon,
+} from 'react-share';
 import './Article.scss';
 import '_markdown_extension.scss';
 
@@ -47,9 +53,22 @@ const Article: FC<Comic> = ({
 
   return (
     <article className="article_container">
-      <Helmet>
-        <title>{title}</title>
-      </Helmet>
+      <Helmet
+        title={title}
+        meta={[
+          { name: 'description', content: subtitle },
+          { name: 'twitter:card', content: 'summary_large_image' },
+          { property: 'og:title', content: title },
+          { property: 'og:type', content: 'article' },
+          { property: 'og:site_name', content: '未定' },
+          { property: 'og:url', content: window.location.href },
+          { property: 'og:image', content: image },
+          {
+            property: 'og:description',
+            content: subtitle,
+          },
+        ]}
+      />
       <div className="genre_icon_group">
         {genres &&
           genres.map((genre) => <GenreIcon key={genre} genre={genre} />)}
@@ -75,7 +94,7 @@ const Article: FC<Comic> = ({
             style={{ width: '100%', display: 'block' }}
             onError={(e) => {
               (e.target as React.ImgHTMLAttributes<HTMLImageElement>).src =
-                'https://charlottech78897cd75f574612ace458f31b6d96a7160346-staging.s3.ap-northeast-1.amazonaws.com/public/IMG_0770.jpg';
+                'IMG_0740.JPG';
             }}
           />
         </div>
@@ -86,8 +105,20 @@ const Article: FC<Comic> = ({
           </div>
         </div>
       </section>
-      <section className="react-split-mde-preview">
-        {markdown}
+      <section className="react-split-mde-preview">{markdown}</section>
+      <section className="share_button_group">
+        <TwitterShareButton
+          url={window.location.href}
+          title={`${subtitle} 「${title}」`}
+        >
+          <TwitterIcon size={30} round />
+        </TwitterShareButton>
+        <FacebookShareButton
+          url={window.location.href}
+          quote={`${subtitle} 「${title}」`}
+        >
+          <FacebookIcon size={30} round />
+        </FacebookShareButton>
       </section>
     </article>
   );
